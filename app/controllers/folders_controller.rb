db148330e4c9afea91e8e33584bc81791401d8c5
Maia Rowe <mrowe7000@icloud.com>
2026-02-17 06:25:49 +0000

    Add Folder controller, routes, and basic views

---
+++ app/controllers/folders_controller.rb
@@ -0,0 +1,31 @@
+class FoldersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @folders = current_user.folders
+  end
+
+  def show
+    @folder = current_user.folders.find(params[:id])
+    @files = @folder.stored_files
+  end
+
+  def new
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = current_user.folders.build(folder_params)
+    if @folder.save
+      redirect_to folders_path, notice: "Folder created successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
- root "home#index" 
- get "home/index" 
- devise_for :users
+  root "home#index"
+  devise_for :users
+
+  authenticate :user do
+    resources :folders, only: [:index, :show, :new, :create]
+  end
 end
 
-
 # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  # get("/your_first_screen", { :controller => "pages", :action => "first" })
+# get("/your_first_screen", { :controller => "pages", :action => "first" })
