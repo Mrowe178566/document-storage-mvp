a5ae79c284a56ae4e89bf1831545b497a61c9656
Maia Rowe <mrowe7000@icloud.com>
2026-02-19 14:49:27 +0000

    Merge main into feature/bulk-delete and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   authenticated :user do
     resources :folders, only: [:index, :show, :new, :create]
     resources :stored_files, only: [:new, :create, :destroy]
-    resources :stored_files, only: [:new, :create, :destroy]
+
+    delete "stored_files/bulk_delete",
+           to: "stored_files#bulk_delete",
+           as: :bulk_delete_stored_files
+
     root "folders#index", as: :authenticated_root
   end
-  unauthenticated do root "home#index" end
+
+  unauthenticated do
+    root "home#index"
+  end
 end
