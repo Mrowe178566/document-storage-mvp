81836c602a28281f1a4215d27a059c590ebdb26d
Maia Rowe <mrowe7000@icloud.com>
2026-02-23 03:28:22 +0000

    Add nested stored_files routes, create upload form partial, clean folders/show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   authenticated :user do
-    resources :folders, only: [:index, :show, :new, :create]
+    resources :folders, only: [:index, :show, :new, :create] do
+      resources :stored_files, only: [:new, :create, :destroy]
+    end
 
     delete "stored_files/bulk_delete",
            to: "stored_files#bulk_delete",
            as: :bulk_delete_stored_files
-
-    resources :stored_files, only: [:new, :create, :destroy]
 
     root "folders#index", as: :authenticated_root
   end
