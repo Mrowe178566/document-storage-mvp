a6eb0e0439c6938269413f40d2b4f62e33cc5795
Maia Rowe <mrowe7000@icloud.com>
2026-04-25 02:43:22 +0000

    test: add model specs for Folder, StoredFile, and User

---
+++ spec/models/folder_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe Folder, type: :model do
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should have_many(:stored_files).dependent(:destroy) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "scopes" do
+    it "returns folders in descending order of creation" do
+      user = User.create!(email: "test@example.com", password: "password")
+      older = user.folders.create!(name: "Older Folder")
+      newer = user.folders.create!(name: "Newer Folder")
+      expect(Folder.recent).to eq([newer, older])
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,11 +48,8 @@ RSpec.configure do |config|
     Rails.application.reload_routes_unless_loaded
   end
 
-  # Shoulda matchers for association accessor specs
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_paths = [ "#{::Rails.root}/spec/fixtures" ]
+  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -81,4 +78,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
